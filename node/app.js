1ccaa6db87c677892d6b580bc837f08a0cbd5ea8
seuqaj114 <migjacques@hotmail.com>
2014-07-21 18:09:43 +0100

    corrected: chat messages being sent to offline users

--- node/app.js
+++ node/app.js
@@ -116,7 +116,7 @@ wss.on('connection', function(ws) {
 
                 console.log("\nUSER "+username+" just entered chat "+data.content.id);
 
-                
+
                 db.collection("files").findOne({_id:new ObjectId(data.content.id)},function(err,file){
                     //console.log(file);
                     console.log("client's chat length: "+data.content.length);
@@ -150,13 +150,15 @@ wss.on('connection', function(ws) {
                 console.log("\nUSER "+username+" just typed a new message to chat "+data.content.id)
                 //redireccionar mensagem para os users do ficheiro
                 for(var i=0;i<data.content.users.length;++i){
-                    openSockets[data.content.users[i]].send(JSON.stringify({
-                        type:"chat-message",
-                        content:{
-                            chat: data.content.chat,
-                            id: data.content.id
-                        }
-                    }));
+                    if(data.content.users[i] in openSockets){
+                        openSockets[data.content.users[i]].send(JSON.stringify({
+                            type:"chat-message",
+                            content:{
+                                chat: data.content.chat,
+                                id: data.content.id
+                            }
+                        }));
+                    }
                 }
 
                 break;
