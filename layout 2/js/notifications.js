36a89bd7b8f8d7cc183a1eb0a9b620c6c38b940c
André Melo <andre.nascimento.melo@gmail.com>
2014-07-22 12:35:43 +0100

    Added websocket notifications.

---
+++ layout 2/js/notifications.js
@@ -0,0 +1,47 @@
+PNotify.prototype.options.styling = "fontawesome";
+
+function webSocketOpenNotification () {
+	PNotify.removeAll();
+
+	$(function(){
+		var notice = new PNotify({
+			title: 'Success!',
+			text: 'Websocket connection was established.',
+			type: 'success',
+			buttons: {
+				sticker: false
+			},
+			mouse_reset: false
+		});
+
+		notice.get().click(function() {
+			notice.remove();
+		});
+	});
+};
+
+function webSocketClosedNotification () {
+	PNotify.removeAll();
+
+	$(function(){
+		var notice = new PNotify({
+			title: 'Darn it!',
+			text: 'Websocket connection was lost. Click me to reconnect.',
+			type: 'error',
+			hide: false,
+			buttons: {
+				sticker: false
+			},
+			mouse_reset: false,
+		});
+
+		notice.get().click(function() {
+			notice.update({
+				title: "Let's try that again." ,
+				text:('Attempting to reconnect...')
+			});
+
+			webSocketConnect();
+		});
+	});
+};
