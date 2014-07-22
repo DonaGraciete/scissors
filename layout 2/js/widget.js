77d9c5641873563680f3e10ac6bc49d4877b67ae
Rodrigo Bernardo <rodrigoambernardo@gmail.com>
2014-07-22 17:22:52 +0100

    Added widget.js

--- layout 2/js/widget.js
+++ layout 2/js/widget.js
@@ -1,1 +1,25 @@
+$(function() {
+
+	$("#priberam-iframe").hide();
+	$("#oxford-iframe").hide();
+
+	$( "#wiki-b" ).click(function() {
+	  $("#wikipedia-iframe").show();
+	  $("#priberam-iframe").hide();
+	  $("#oxford-iframe").hide();
 	});
+
+	$( "#pri-b" ).click(function() {
+	  $("#wikipedia-iframe").hide();
+	  $("#priberam-iframe").show();
+	  $("#oxford-iframe").hide();
+	});
+
+	$( "#ox-b" ).click(function() {
+	  $("#wikipedia-iframe").hide();
+	  $("#priberam-iframe").hide();
+	  $("#oxford-iframe").show();
+	});
+
+});
+
