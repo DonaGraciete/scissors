1b68b293d5f12e5e68b16470f212431040008f78
seuqaj114 <migjacques@hotmail.com>
2014-07-22 16:26:02 +0100

    added login.js

---
+++ layout 2/js/login.js
@@ -0,0 +1,95 @@
+var options = {
+	"show": "true",
+	"backdrop" : "false"
+};
+
+$("#login-modal").modal(options);
+
+$("#create-account").click(function (event) {
+	$(".login-form").addClass('hidden');
+	$(".register-form").removeClass('hidden');
+	$("#error-message").text("");
+});
+
+$("#switch-to-sign-in").click(function (event) {
+	$(".register-form").addClass('hidden');
+	$(".login-form").removeClass('hidden');
+	$("#error-message").text("");
+});
+
+$("#form-signin").submit(function(event){
+	event.preventDefault();
+
+	var loginUsername = $("input[name=username-signin]").val();
+	var loginPassword = $("input[name=password-signin]").val();
+
+	JSONdata = JSON.stringify({
+		username:loginUsername,
+		password:loginPassword
+	});
+
+	$.ajax({
+		url: apiUrl+"login",
+		type: "POST",
+		data: JSONdata,
+		datatype : "json",
+    	contentType: "application/json; charset=utf-8",
+		success: function(data){
+			console.log("success");
+			if(data.result == true){
+				username = loginUsername;
+				$("#login-modal").modal("hide");
+				webSocketConnect();
+			}
+			else{
+				console.log("informacao errada");
+				$("#error-message").text("User credentials incorrect");
+			}
+		},
+		error: function(xhr, tetStatus, errorThrown){
+			$("#error-message").text("Sorry, an error happened: "+xhr.responseText);
+		}
+	});
+
+});
+
+$("#form-signup").submit(function(event){
+	event.preventDefault();
+	
+	var loginUsername = $("input[name=username-signup]").val();
+	var loginPassword = $("input[name=password-signup]").val();
+	var loginConfirm = $("input[name=password-confirm]").val();
+
+	if(loginPassword != loginConfirm){
+		$("#error-message").text("Passwords don't match");
+		return;
+	}
+
+	JSONdata = JSON.stringify({
+		username:loginUsername,
+		password:loginPassword
+	});
+
+	$.ajax({
+		url: apiUrl+"register",
+		type: "POST",
+		data: JSONdata,
+		datatype : "json",
+    	contentType: "application/json; charset=utf-8",
+		success: function(data){
+			console.log("success");
+			if(data.result == true){
+				username = loginUsername;
+				webSocketConnect();
+			}
+			else{
+				console.log("informacao errada");
+				$("#error-message").text("Username already taken");
+			}
+		},
+		error: function(xhr, tetStatus, errorThrown){
+			$("#error-message").text("Sorry, an error happened: "+xhr.responseText);
+		}
+	});
+
+});
