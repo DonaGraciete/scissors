5b08f46d4d61119a9c1bea4544740628a408a4cc
Miguel Araújo <miguel.f.araujo@tecnico.ulisboa.pt>
2014-07-23 16:38:08 +0100

    Add yahoo widgets

--- layout 2/js/widget.js
+++ layout 2/js/widget.js
@@ -2,24 +2,36 @@ $(function() {
 
 	$("#priberam-iframe").hide();
 	$("#oxford-iframe").hide();
+	$("#yahoo-iframe").hide();
 
 	$( "#wiki-b" ).click(function() {
 	  $("#wikipedia-iframe").show();
 	  $("#priberam-iframe").hide();
 	  $("#oxford-iframe").hide();
+	  $("#yahoo-iframe").hide();
 	});
 
 	$( "#pri-b" ).click(function() {
 	  $("#wikipedia-iframe").hide();
 	  $("#priberam-iframe").show();
 	  $("#oxford-iframe").hide();
+	  $("#yahoo-iframe").hide();
 	});
 
 	$( "#ox-b" ).click(function() {
 	  $("#wikipedia-iframe").hide();
 	  $("#priberam-iframe").hide();
 	  $("#oxford-iframe").show();
+	  $("#yahoo-iframe").hide();
 	});
+
+	$( "#yahoo-b" ).click(function() {
+	  $("#wikipedia-iframe").hide();
+	  $("#priberam-iframe").hide();
+	  $("#oxford-iframe").hide();
+	  $("#yahoo-iframe").show();
+	});
+
 
 });
 
