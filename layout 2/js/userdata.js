1581d14fdf3fe04c29b612a62819f09dccd4b089
seuqaj114 <migjacques@hotmail.com>
2014-07-25 02:00:48 +0100

    fixe

--- layout 2/js/userdata.js
+++ layout 2/js/userdata.js
@@ -3,9 +3,10 @@ var username;
 var userId;
 var files = [];
 var fileChatInUse={};
-/*
+
 var url = "ws://188.226.234.189:3000/";
 var apiUrl = "http://188.226.234.189:3000/api/"
-*/
+/*
 var url = "ws://localhost:3000/";
 var apiUrl = "http://localhost:3000/api/"
+*/
